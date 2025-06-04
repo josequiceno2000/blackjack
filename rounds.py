6bccab3107304e595353bfa0b5b8130680d2fe5c
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-06-03 22:10:06 -0500

    Add arrays for player and computer cards

--- rounds.py
+++ rounds.py
@@ -2,7 +2,10 @@ import random
 
 def play_round():
     # Create a card array ✔
+    # Create arrays for player and computer ✔
     possible_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    player_cards = []
+    computer_cards = []
 
     # Choose a random card ✔
     return random.choice(possible_cards)
