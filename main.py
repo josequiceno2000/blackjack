9c2a818dd508099b2cea041c1656a2131146866a
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-06-03 22:08:55 -0500

    Choose random cards

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from intro import play_blackjack, title_card
+from rounds import play_round
 
 def main():
     if play_blackjack():
         title_card()
+        print(play_round())
 
 if __name__ == "__main__":
     main()
--- rounds.py
+++ rounds.py
@@ -1,4 +1,8 @@
+import random
 
 def play_round():
     # Create a card array ✔
     possible_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+    # Choose a random card ✔
+    return random.choice(possible_cards)
